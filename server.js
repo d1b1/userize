f9cf1f0d6af3c9c00cd37599662662e2d7a8a389
Stephan Smith <steve@soend.com>
2012-06-27 09:51:25 -0400

    Add heroku config

---
+++ server.js
@@ -0,0 +1,30 @@
+var express    = require('express'),
+    requestObj = require('request'),
+    http       = require('http'),
+    crypto     = require('crypto');
+
+var userize = require('./userize');
+
+// ---------------------------------------------------
+
+var app = express.createServer( express.logger() );
+
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+});
+
+// The following are standard user authentication and
+// profile management express paths.
+
+app.get('/api/profile/:user',      userize.profile );
+app.put('/api/profile/:user',      userize.updateProfile );
+app.post('/api/register',          userize.register );
+app.post('/api/login',             userize.login );
+app.post('/api/logout',            userize.logout );
+
+var port = process.env.PORT || 4000;
+app.listen(port, function() { 
+  console.log("StartUp: Userize" + port ); 
+});
